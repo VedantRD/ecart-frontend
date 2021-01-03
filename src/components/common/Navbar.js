1ee1b99e183c28bb8ceb58cf9690abaf912dbae2
VedantRD <vedant.debadwar@gmail.com>
2021-01-03 20:16:43 +0530

    styled navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -2,10 +2,12 @@ import React, { useContext } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { UserContext } from '../../App';
 import '../../styles/common/navbar.css'
+import Searchbar from './Searchbar';
 
 const Navbar = () => {
 
     const { state, dispatch } = useContext(UserContext)
+    const homeRoute = (state && state.type === 'seller') ? '/seller' : '/buyer'
     const history = useHistory()
 
     const logout = () => {
@@ -14,22 +16,57 @@ const Navbar = () => {
         history.push('/buyer/login')
     }
 
+
     return (
         <div className='myNavbar'>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-                <Link className="navbar-brand" to='/'>Navbar</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top px-md-5 px-3">
+
+                {/* logo */}
+                <Link className="navbar-brand" to={homeRoute}>Navbar</Link>
+
+                {/* collapsible button */}
+                <button className="navbar-toggler" type="button"
+                    data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
+                {/* routes */}
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <Link className="nav-item nav-link active" to='/'>Home <span className="sr-only">(current)</span></Link>
-                        <Link className="nav-item nav-link" to='/'>About Us</Link>
-                        <Link className="nav-item nav-link" to='/'>Contact</Link>
-                        {state && <Link className="nav-item nav-link" to='/' onClick={logout}>Logout</Link>}
-                        {state === null && <Link className="nav-item nav-link" to='/buyer/login'>Login</Link>}
+                    <div className="navbar-nav w-100">
+
+                        {/* links */}
+                        <Link className="nav-item nav-link ml-md-5" to={homeRoute}>Home</Link>
+                        <Link className="nav-item nav-link ml-md-3" to='/'>About Us</Link>
+                        <Link className="nav-item nav-link ml-md-3" to='/'>Contact</Link>
+
+                        {/* searchbar */}
+                        <Searchbar />
+
+                        {/* login & logout */}
+                        {state &&
+                            <Link
+                                className="nav-item nav-link py-0 mt-md-0 mt-3 px-0"
+                                to='/' onClick={logout}
+                            >
+                                <button className='btn btn-danger px-5 btn-shadow-1'>
+                                    Logout
+                                </button>
+                            </Link>
+                        }
+                        {state === null &&
+                            <Link className="nav-item nav-link py-0 mt-md-0 mt-3 px-0"
+                                to='/buyer/login'
+                            >
+                                <button className='btn btn-success px-5 rounded-0 btn-shadow-1'>
+                                    Login
+                                </button>
+                            </Link>
+                        }
+
                     </div>
                 </div>
+
             </nav>
         </div>
     )
---
+++ src/components/common/Searchbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../styles/common/searchbar.css'
+
+const Searchbar = () => {
+    return (
+        <form className="form-inline ml-auto mySearchbar">
+            <input
+                className="form-control w-75" type="search"
+                placeholder="Search Product Here" aria-label="Search"
+                style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
+            />
+            <button
+                className="btn btn-success w-25" type="submit"
+                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+            >
+                Search
+            </button>
+        </form>
+    )
+}
+
+export default Searchbar
